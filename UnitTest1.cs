9ddcc6220202e5ae9ffdc158f99bdbfa68b2b50a
raghav sharma <raghavtyagiofficial07@gmail.com>
2024-10-03 16:50:20 +1000

    Initial commit

---
+++ UnitTest1.cs
@@ -0,0 +1,98 @@
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using System;
+
+namespace TestProject5
+{
+    public class Program
+    {
+
+        private IWebDriver driver;
+
+        [SetUp]
+        public void Setup()
+        {
+            driver = new ChromeDriver();
+            driver.Manage().Window.Maximize();
+        }
+
+        [Test]
+        public void Test1()
+        {
+            driver.Navigate().GoToUrl("https://app-linux-test-curious4ever-user.azurewebsites.net/registration");
+            System.Threading.Thread.Sleep(2000);
+            IWebElement fullnameField = driver.FindElement(By.Id("txtFullName"));
+            IWebElement emailField = driver.FindElement(By.Id("txtEmailId"));
+            
+            IWebElement registerButton = driver.FindElement(By.Id("btnRegister"));
+
+            fullnameField.SendKeys("abc");
+            
+            registerButton.Click();
+            string answer = null;
+
+            Console.WriteLine(emailField.Text);
+           
+            if (emailField.Text == "")
+            {
+                WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+                wait.Until(drv => drv.FindElement(By.CssSelector("span.error-message.alert.alert-danger.d-block")).Displayed);
+                IWebElement errorMessage = driver.FindElement(By.CssSelector("span.error-message.alert.alert-danger.d-block"));
+                Console.WriteLine("Error Message: " + errorMessage.Text);
+                answer = errorMessage.Text;
+            }
+            else
+            {
+                answer = null;
+            }
+
+            Assert.That(answer, Is.EqualTo("Please mention your email id."));
+        }
+
+        [Test]
+        public void Test2()
+        {
+            driver.Navigate().GoToUrl("https://app-linux-test-curious4ever-user.azurewebsites.net/registration");
+            System.Threading.Thread.Sleep(2000);
+            IWebElement fullnameField = driver.FindElement(By.Id("txtFullName"));
+            IWebElement emailField = driver.FindElement(By.Id("txtEmailId"));
+            IWebElement registerButton = driver.FindElement(By.Id("btnRegister"));
+
+            registerButton.Click();
+            string answer = null;
+
+            if (emailField.Text == "")
+            {
+               
+                WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+                wait.Until(drv => drv.FindElement(By.CssSelector("span.error-message.alert.alert-danger.d-block")).Displayed);
+                IWebElement errorMessage = driver.FindElement(By.CssSelector("span.error-message.alert.alert-danger.d-block"));
+                Console.WriteLine("Error Message: " + errorMessage.Text);
+                answer = errorMessage.Text;
+            }
+            else
+            {
+                answer = null;
+
+
+            }
+
+
+            Assert.That(answer, Is.EqualTo("Please mention your name."));
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            // Check if driver is not null before quitting
+            if (driver != null)
+            {
+                driver.Quit();
+                driver.Dispose(); // Dispose of the driver
+            }
+        }
+
+
+    }
+}
